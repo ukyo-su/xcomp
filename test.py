5c323d239d2f0ade1f35594814a79efd0a57da1b
ukyo-su <mns.mna.otb.btb@gmail.com>
2020-05-02 01:10:04 +0900

    comp&reduce_comp&delay_arg

---
+++ test.py
@@ -0,0 +1,111 @@
+from xcomp import (comp, multi_comp, delay_arg, multi_reduce_comp, reduce_comp,
+                   BreakReduce, ContinueReduce)
+
+
+def test_list_comp():
+    @comp(list)
+    def actual():
+        for i in range(5):
+            yield i
+
+    assert actual == list(range(5))
+
+
+def test_dict_comp():
+    @comp(dict)
+    def actual():
+        for i in range(5):
+            yield str(i), i
+
+    expected = {str(i): i for i in range(5)}
+
+    assert actual == expected
+
+
+def test_sum_comp():
+    @comp(sum)
+    def actual():
+        for i in range(5):
+            yield i
+
+    assert actual == sum(range(5))
+
+
+def test_sum_list_comp():
+    @multi_comp(sum, list)
+    def actual():
+        for i in range(5):
+            yield i, i * 2
+
+    actual_sum, actual_list = actual
+    assert actual_sum == sum(range(5))
+    assert actual_list == [i * 2 for i in range(5)]
+
+
+def test_sum_reduce_comp():
+    @reduce_comp(0, iter_=range(5))
+    def actual(sum_, i):
+        return sum_ + i
+
+    assert actual == sum(range(5))
+
+
+def test_sum_rev_list_multi_reduce_comp():
+    @multi_reduce_comp((0, []),
+                       iter_=range(5))
+    def actual(sum_, rev_list, i):
+        return sum_ + i, [i, *rev_list]
+
+    actual_sum, actual_rev_list = actual
+
+    assert actual_sum == sum(range(5))
+    assert actual_rev_list == list(reversed(range(5)))
+
+
+def test_unique_multi_reduce_comp():
+    @multi_reduce_comp(([], set()),
+                       iter_=[0, 1, 1, 2, 3, 4, 4, 4],
+                       result=lambda acc, seen: list(reversed(acc)))
+    def actual(acc, seen, i):
+        if i in seen:
+            return acc, seen
+        else:
+            return [i, *acc], {*seen, i}
+
+    assert actual == [0, 1, 2, 3, 4]
+
+
+def test_break_reduce_comp():
+    @reduce_comp(0, iter_=range(100))
+    def actual(acc, i):
+        if i > 5:
+            raise BreakReduce
+        return acc + 1
+
+    assert actual == 6
+
+
+def test_continue_reduce_comp():
+    @reduce_comp(0, iter_=range(10))
+    def actual(acc, i):
+        if i < 5:
+            raise ContinueReduce
+        return acc + i
+
+    assert actual == sum(range(5, 10))
+
+
+def test_nest_reduce_comp():
+    @reduce_comp([], iters=(range(3), range(2),))
+    def actual(acc, i, j):
+        return [*acc, (i, j)]
+
+    assert actual == [(i, j) for i in range(3) for j in range(2)]
+
+
+def test_map_delay_arg():
+    @delay_arg(map, range(5))
+    def actual(i):
+        return i * 2
+
+    assert list(actual) == list(map(lambda i: i * 2, range(5)))
---
+++ xcomp.py
@@ -0,0 +1,61 @@
+from itertools import product
+from contextlib import contextmanager
+
+
+def comp(collector):
+    def decorator(func):
+        return collector(func())
+    return decorator
+
+
+def multi_comp(*collectors):
+    def decorator(func):
+        return tuple(c(v) for c, v in zip(collectors, zip(*func())))
+    return decorator
+
+
+class BreakReduce(Exception):
+    pass
+
+
+class ContinueReduce(Exception):
+    pass
+
+
+@contextmanager
+def ignore(exception):
+    try:
+        yield
+    except exception:
+        pass
+
+
+def multi_reduce_comp(inits=(), iters=(), *, iter_=None,
+                      result=lambda *args: args):
+    if iter_ is not None:
+        iters = (iter_,)
+
+    def decorator(func):
+        accums = inits
+        with ignore(BreakReduce):
+            for vs in product(*iters):
+                with ignore(ContinueReduce):
+                    accums = func(*accums, *vs)
+        return result(*accums)
+    return decorator
+
+
+def reduce_comp(init, iters=(), *, iter_=None,
+                result=lambda *args: args):
+    def decorator(func):
+        def multi(*args):
+            return func(*args),
+        return multi_reduce_comp(inits=(init,), iters=iters,
+                                 iter_=iter_, result=result)(multi)[0]
+    return decorator
+
+
+def delay_arg(func, *args, **kwargs):
+    def delayed(arg):
+        return func(arg, *args, **kwargs)
+    return delayed
