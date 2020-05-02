89842d1e5cbcda89d9c4949f798d51c75eb8c30c
ukyo-su <mns.mna.otb.btb@gmail.com>
2020-05-02 12:13:20 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="xcomp",
+    version='1.0',
+    description='Extended comprehension, inspired by Racket for-syntax',
+    author='Ukyo Suzuki',
+    author_email='mns.mna.otb.btb@gmail.com',
+    url='https://github.com/ukyo-su/xcomp',
+    py_modules=['xcomp']
+)
