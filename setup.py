34f9ce2f6086894a4d73b7cbbe08331d9b72d7c7
ukyo-su <mns.mna.otb.btb@gmail.com>
2020-05-05 22:56:29 +0900

    typo

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name="xcomp",
-    version='1.0',
-    description='Extended comprehension, inspired by Racket for-syntax',
-    author='Ukyo Suzuki',
-    author_email='mns.mna.otb.btb@gmail.com',
-    url='https://github.com/ukyo-su/xcomp',
-    py_modules=['xcomp']
+    version="1.0",
+    description="Extended comprehension, inspired by Racket for-syntax",
+    author="Ukyo Suzuki",
+    author_email="mns.mna.otb.btb@gmail.com",
+    url="https://github.com/ukyo-su/xcomp",
+    py_modules=["xcomp"]
 )
